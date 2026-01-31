6f6673f82131714bab65a252b1481e0c30701b46
Fhi951 <lfebriannoor@gmail.com>
2026-01-31 23:40:48 +0700

    finalize

--- config/database.php
+++ config/database.php
@@ -1,11 +1,11 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "uas_pemrograman_web";
+$host = "sql201.infinityfree.com";
+$user = "if0_41031719";
+$pass = "Uqda8a4w0su";
+$db   = "if0_41031719_db_pengaduan";
 
 $conn = mysqli_connect($host, $user, $pass, $db);
 
 if (!$conn) {
-    die("Koneksi database gagal: " . mysqli_connect_error());
+    die("Koneksi database gagal");
 }
--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php
-session_start();
-
-if (isset($_SESSION['login'])) {
-    if ($_SESSION['role'] == 'admin') {
-        header("Location: admin/dashboard.php");
-    } else {
-        header("Location: user/dashboard.php");
-    }
-} else {
-    header("Location: auth/login.php");
-}
+header("Location: auth/login.php");
 exit;
